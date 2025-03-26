12cc820d29a5387de87618db272b9ee0483ff5d0
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2025-03-27 00:32:55 +0200

    corrected bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,11 +62,12 @@ export default function TodoListApp () {
   if (error) return <p>{error}</p>;
 
   return (
-      <div className="p-6 mx-auto bg-white shadow-md space-y-4 h-screen flex flex-col">
-        <h1 className="text-2xl font-bold text-center text-gray-700">Todo App</h1>
+      <div className="p-6 mx-auto bg-white shadow-md space-y-4 h-screen flex flex-col justify-center">
+        <h1 className="text-2xl font-bold text-center text-gray-700">Todo List</h1>
         <div className="flex space-x-2">
           <input
               type="text"
+              placeholder="New task description..."
               className="flex-1 bg-gray-200 rounded-md text-gray-700 p-2 transition-all focus:bg-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
               value={newTodo}
               onChange={(e) => setNewTodo(e.target.value)}
@@ -78,23 +79,24 @@ export default function TodoListApp () {
             Add
           </button>
         </div>
-        <div className="flex-1 overflow-y-auto">
-          <ul className="flex flex-wrap gap-4 justify-space-between">
+        <div className="flex-1 overflow-y-auto" >
+          <ul className="flex flex-wrap gap-5 justify-start w-full max-w-full">
             {todos.map((todo) => (
                 <li
                     key={todo.id}
-                    className={`w-[18%] min-w-[200px] p-4 flex flex-col place-content-between rounded-md ${todo.completed ? "bg-emerald-200" : "bg-sky-200"}`}
+                    className={`w-[calc(100%/5-1rem)] min-w-[200px] p-4 flex flex-col place-content-between rounded-md ${todo.completed ? "bg-emerald-200" : "bg-sky-200"}`}
                 >
+                  <span className="text-gray-700 break-words">{todo.title}</span>
                   <div>
-                    <span className="text-gray-700">{todo.title}</span>
-                    <span className="text-gray-400 block text-sm">{todo.completed ? "Status: Completed" : "Status: In Progress"}</span>
+                    <span
+                        className="mt-5 text-gray-400 block text-sm">{todo.completed ? "Status: Completed" : "Status: In Progress"}</span>
+                    <button
+                        className="w-full bg-red-400 text-white mt-1 px-2 py-1 rounded transition-all ease-in-out duration-500 hover:bg-red-600"
+                        onClick={() => deleteTodo(todo.id)}
+                    >
+                      Delete
+                    </button>
                   </div>
-                  <button
-                      className="bg-red-400 text-white mt-10 px-2 py-1 rounded transition-all ease-in-out duration-500 hover:bg-blue-500"
-                      onClick={() => deleteTodo(todo.id)}
-                  >
-                    Delete
-                  </button>
                 </li>
             ))}
           </ul>
