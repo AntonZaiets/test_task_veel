adbf2a20fd256c7f3f6291d22d655af4f3fbb514
AntonZaiets <133760646+AntonZaiets@users.noreply.github.com>
2025-03-28 19:44:28 +0200

    fixed bugs

--- src/app/utils/QueryProvider.tsx
+++ src/app/utils/QueryProvider.tsx
@@ -10,7 +10,7 @@ export default function QueryProvider({ children }: { children: React.ReactNode 
     const [queryClient] = useState(() => new QueryClient({
         defaultOptions: {
             queries: {
-                staleTime: 1000 * 60 * 60, // 1 hour
+                staleTime: 1000 * 60 * 60,
             },
         },
     }));
